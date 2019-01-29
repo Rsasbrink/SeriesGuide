a4460ec14684bfca960f1c508b3ed8fb8629b561
René Sasbrink <R.sasbrink@live.nl>
2019-01-29 17:12:56 +0100

    added more tests

--- app/src/androidTest/java/com/battlelancer/seriesguide/ui/AddDeleteSearchSeries.java
+++ app/src/androidTest/java/com/battlelancer/seriesguide/ui/AddDeleteSearchSeries.java
@@ -10,10 +10,11 @@ import com.battlelancer.seriesguide.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
@@ -33,11 +34,12 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class SearchForShow {
-    String title = "Friends";
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AddDeleteSearchSeries {
+String title = "Friends";
     @Rule
     public ActivityTestRule<ShowsActivity> mActivityTestRule = new ActivityTestRule<>(ShowsActivity.class);
 
@@ -107,7 +109,6 @@ public class SearchForShow {
 
         ViewInteraction textView = onView(
                 allOf(withId(R.id.textViewAddTitle),
-
                         isDisplayed()));
         try {
             Thread.sleep(7000);
@@ -117,6 +118,115 @@ public class SearchForShow {
         textView.check(matches(withText(title)));
     }
 
+    @Test
+    public void addSeries() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.buttonShowsAdd), withContentDescription("Serie toevoegen"),
+                        childAtPosition(
+                                allOf(withId(R.id.rootLayoutShows),
+                                        childAtPosition(
+                                                withId(R.id.drawer_layout),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatAutoCompleteTextView = onView(
+                allOf(withId(R.id.editTextSearchBar),
+                        childAtPosition(
+                                allOf(withId(R.id.containerSearchBar),
+                                        childAtPosition(
+                                                withId(R.id.sgToolbar),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView.perform(replaceText("Friends"), closeSoftKeyboard());
+
+        ViewInteraction appCompatAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.editTextSearchBar), withText("Friends"),
+                        childAtPosition(
+                                allOf(withId(R.id.containerSearchBar),
+                                        childAtPosition(
+                                                withId(R.id.sgToolbar),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatAutoCompleteTextView2.perform(pressImeActionButton());
+
+        ViewInteraction frameLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.recyclerViewShowsDiscover),
+                                childAtPosition(
+                                        withId(R.id.constraintLayoutShowsDiscover),
+                                        1)),
+                        0),
+                        isDisplayed()));
+        frameLayout.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.buttonPositive), withText("Serie toevoegen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+    @Test
+    public void deleteSeries() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageView2 = onView(
+                allOf(withId(R.id.imageViewShowsContextMenu), withContentDescription("Meer opties"),
+                        isDisplayed()));
+        appCompatImageView2.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.title), withText("Verwijderen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.view.menu.ListMenuItemView")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.buttonPositive), withText("Verwijderen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+    }
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
---
+++ app/src/androidTest/java/com/battlelancer/seriesguide/ui/FavouriteSeries.java
@@ -0,0 +1,260 @@
+package com.battlelancer.seriesguide.ui;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.battlelancer.seriesguide.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class FavouriteSeries {
+
+    @Rule
+    public ActivityTestRule<ShowsActivity> mActivityTestRule = new ActivityTestRule<>(ShowsActivity.class);
+
+    @Test
+    public void favouriteSeries() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.buttonShowsAdd), withContentDescription("Serie toevoegen"),
+                        childAtPosition(
+                                allOf(withId(R.id.rootLayoutShows),
+                                        childAtPosition(
+                                                withId(R.id.drawer_layout),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction frameLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.recyclerViewShowsDiscover),
+                                childAtPosition(
+                                        withId(R.id.constraintLayoutShowsDiscover),
+                                        1)),
+                        2),
+                        isDisplayed()));
+        frameLayout.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.buttonPositive), withText("Serie toevoegen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        2),
+                                2),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Omhoog navigeren"),
+                        childAtPosition(
+                                allOf(withId(R.id.sgToolbar),
+                                        childAtPosition(
+                                                withClassName(is("com.google.android.material.appbar.AppBarLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction constraintLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.recyclerViewShows),
+                                childAtPosition(
+                                        withId(R.id.shows_fragment),
+                                        2)),
+                        1),
+                        isDisplayed()));
+        constraintLayout.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.imageButtonFavorite), withContentDescription("Toevoegen aan favorieten"),
+                        childAtPosition(
+                                allOf(withId(R.id.containerOverviewShow),
+                                        childAtPosition(
+                                                withId(R.id.overview_container),
+                                                0)),
+                                1)));
+        appCompatImageButton2.perform(scrollTo(), click());
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.imageButtonFavorite), withContentDescription("Verwijderen uit favorieten"),
+                        childAtPosition(
+                                allOf(withId(R.id.containerOverviewShow),
+                                        childAtPosition(
+                                                withId(R.id.overview_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withId(R.id.imageButtonFavorite), withContentDescription("Verwijderen uit favorieten"),
+                        childAtPosition(
+                                allOf(withId(R.id.containerOverviewShow),
+                                        childAtPosition(
+                                                withId(R.id.overview_container),
+                                                0)),
+                                1)));
+        appCompatImageButton3.perform(scrollTo(), click());
+
+        ViewInteraction imageButton2 = onView(
+                allOf(withId(R.id.imageButtonFavorite), withContentDescription("Toevoegen aan favorieten"),
+                        childAtPosition(
+                                allOf(withId(R.id.containerOverviewShow),
+                                        childAtPosition(
+                                                withId(R.id.overview_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        imageButton2.check(matches(isDisplayed()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withContentDescription("Omhoog navigeren"),
+                        childAtPosition(
+                                allOf(withId(R.id.sgToolbar),
+                                        childAtPosition(
+                                                withClassName(is("com.google.android.material.appbar.AppBarLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.imageViewShowsContextMenu), withContentDescription("Meer opties"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recyclerViewShows),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(android.R.id.title), withText("Verwijderen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.android.internal.view.menu.ListMenuItemView")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.buttonPositive), withText("Verwijderen"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
